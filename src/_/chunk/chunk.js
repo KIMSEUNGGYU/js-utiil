99061b9b65fd7795a959417dc320c7c00a631fa6
kimseunggyu <dev.seunggyu@gmail.com>
2022-04-11 12:06:27 +0900

    chunk 기능 구현

--- src/_/chunk/chunk.js
+++ src/_/chunk/chunk.js
@@ -5,6 +5,14 @@ const _ = require('lodash');
 function chunk(array, size = 1) {
   const newArray = [];
 
+  if (typeof array === 'undefined' || array === null) {
+    throw Error('array 인자는 필수입니다.');
+  }
+
+  if (!Array.isArray(array)) {
+    throw Error('array 인자는 array 타입만 가능합니다');
+  }
+
   let index = 0;
   while (index < array.length) {
     newArray.push(array.slice(index, index + size));
