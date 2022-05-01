a0e727688311549be9820ff3de6ff5ac28d9b1b8
kimseunggyu <dev.seunggyu@gmail.com>
2022-05-01 20:37:47 +0900

    [docs] 피드백 작성

--- src/_/cloneDeep/cloneDeep.test.js
+++ src/_/cloneDeep/cloneDeep.test.js
@@ -3,22 +3,62 @@ const _ = require('lodash');
 const { cloneDeep } = require('./cloneDeep');
 const { deepCloneWeakMap } = require('./cloneDeepWeakmap');
 
+// ncc - babel 같이 playground 설정 돕는 tool
+
+// expect.extend({
+//   deepEqual(received, expected) {
+//     console.log(this);
+//     return this.equals(received, expected)
+//   }
+// })
+
 describe('cloneDeep', () => {
   it('object', () => {
     const object = {
       a: 1,
       b: 2,
       c: {
-        d: 3,
+        // d: 3,
+        d: {},
       },
     };
 
     const copyObject = cloneDeep(object);
 
+    // shallow copy test 코드 같음
+    // shallow copy 에도 동작할 거 같음! ()
+
+    // expect(object).d
     expect(object).not.toBe(copyObject);
     expect(object.a).toBe(copyObject.a);
     expect(object.c).not.toBe(copyObject.c);
+    expect(object.c.d).not.toBe(copyObject.c.d); // 방법 1 deep copy test
+
+
+    // const a = { b: 1 }
+    // const b = { b: 1 }
+    // expect(a).toBe(b)
+    // expect(object).toEqual(copyObject)
+    // expect(a).toEqual(b)
+    // expect(object).deepEqual(object, copyObject)
+    // 방법 2
+    // https://jestjs.io/docs/expect#thisequalsa-b
+    // this - equal
   });
+
+  it('object - this.equals', () => {
+    const object = {
+      a: 1,
+      b: 2,
+      c: {
+        // d: 3,
+        d: {},
+      },
+    };
+
+    const copyObject = cloneDeep(object);
+    expect(copyObject).eq
+  })
 
   it('object - weakMap', () => {
     const object = {
--- src/_/memoize/memoize2.js
+++ src/_/memoize/memoize2.js
@@ -2,23 +2,29 @@
 
 function memoize2(func) {
     const cache = new WeakMap();
+    // let cache = null
     // 외부에서 캐시 값 접근 가능하기 위해 prototype 에 추가
     // 테스트 시 캐시 제거용, lodash 에서도 캐시에 접근 가능하게 구성되어 있음.
     func.__proto__.cache = cache;
 
     return function (...args) {
-        const key = args[0]
+        // { ...args } 는 새로운 객체를 가져와서 문제... (다른 값으로 인식)
+        // const key = typeof args[0] === 'object' ? args[0] : { ...args }
+        // const key = typeof args[0] === 'object' ? args[0] : args
+        const key = typeof args[0] === 'object' ? args[0] : args
 
         // 해당 memoize 는 단일 인자만 처리
-        if(args.length !== 1) {
-            return func(...args)
-        }
+        // if(args.length !== 1) {
+        //     return func(...args)
+        // }
 
+        // console.log('key', key, cache.get(key));
         if(cache.has(key)) {
             return cache.get(key);
         }
 
         const result = func(...args);
+        console.log('call', key, result);
         cache.set(key, result);
 
         return result;
